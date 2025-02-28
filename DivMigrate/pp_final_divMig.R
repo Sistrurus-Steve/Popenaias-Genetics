63fe424b670f8c0d1fc00c9b38a841cb67e5cd56
StevenRHein <91970313+Sistrurus-Steve@users.noreply.github.com>
2025-02-28 14:44:40 -0500

    Add files via upload

---
+++ DivMigrate/pp_final_divMig.R
@@ -0,0 +1,19 @@
+library('diveRsity')
+
+setwd("/media/stevehein/SteveHeinDATA/Popenaias/final_data/anaylses/DivMig/")
+
+divMigrate(infile = "neutral_pp_snps.gen", outfile = "divMig_0_neutral", boots = 1000, stat = "all",
+           filter_threshold = 0, plot_network = TRUE, plot_col = "darkblue", para = TRUE)
+
+divMigrate(infile = "neutral_pp_snps.gen", outfile = "divMig_25_neutral", boots = 1000, stat = "all",
+           filter_threshold = .25, plot_network = TRUE, plot_col = "darkblue", para = TRUE)
+
+divMigrate(infile = "neutral_pp_snps.gen", outfile = "divMig_40_neutral", boots = 1000, stat = "all",
+           filter_threshold = .40, plot_network = TRUE, plot_col = "darkblue", para = TRUE)
+
+divMigrate(infile = "neutral_pp_snps.gen", outfile = "divMig_35_neutral", boots = 1000, stat = "all",
+           filter_threshold = .35, plot_network = TRUE, plot_col = "darkblue", para = TRUE)
+
+
+
+
